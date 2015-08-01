8bc2bfe84babdb3283de3ad87b75b2951702ef62
wordsforthewise <wordsforthewise@gmail.com>
2015-07-31 21:56:58 -0700

    added concentration data

--- arduino+esp01/save arduino data.py
+++ arduino+esp01/save arduino data.py
@@ -6,25 +6,24 @@ ser = serial.Serial(1,timeout=4)
 datafile = 'arduino data.csv'
 P1ratio = None
 if not os.path.isfile(datafile):
-    with open(datafile,'a+') as csvfile:
+    with open(datafile,'wb') as csvfile:
         arduinocsv = csv.writer(csvfile, delimiter = ',')
-        arduinocsv.writerow(['0.5um', '1um', 'time (iso)'])
+        arduinocsv.writerow(['P2 ratio', 'P1 ratio', '0.5um', '1um', 'time (iso)'])
 
-with open(datafile,'a+') as csvfile:
-    arduinocsv = csv.writer(csvfile, delimiter = ',')
-    while True:
-        line = ser.readline()
-        if line!='':
-            print line
-            if re.search('.*P1: (\d+\.\d+).*', line):
-                P1conc = re.search('.*P1: (\d+\.\d+).*', line).group(1)
-            if re.search('.*P1 ratio: (\d+\.\d+).*', line):
-                P1ratio = re.search('.*P1 ratio: (\d+\.\d+).*', line).group(1)
-            if re.search('.*P2: (\d+\.\d+).*', line):
-                P2conc = re.search('.*P2: (\d+\.\d+).*', line).group(1)
-            measureTime = datetime.now().isoformat()
-            if re.search('.*P2 ratio: (\d+\.\d+).*', line) and P1ratio != None:
-                P2ratio = re.search('.*P2 ratio: (\d+\.\d+).*', line).group(1)
-                print 'P1, P2 ratios:', P1ratio, P2ratio
-                print 'P1, P2 concs:', P1conc, P2conc
-                arduinocsv.writerow([P2ratio, P1ratio, measureTime])
+while True:
+    line = ser.readline()
+    if line!='':
+        if re.search('.*P1: (\d+\.\d+).*', line):
+            P1conc = re.search('.*P1: (\d+\.\d+).*', line).group(1)
+        if re.search('.*P1 ratio: (\d+\.\d+).*', line):
+            P1ratio = re.search('.*P1 ratio: (\d+\.\d+).*', line).group(1)
+        if re.search('.*P2: (\d+\.\d+).*', line):
+            P2conc = re.search('.*P2: (\d+\.\d+).*', line).group(1)
+        measureTime = datetime.now().isoformat()
+        if re.search('.*P2 ratio: (\d+\.\d+).*', line) and P1ratio != None:
+            P2ratio = re.search('.*P2 ratio: (\d+\.\d+).*', line).group(1)
+            print 'P1, P2 ratios:', P1ratio, P2ratio
+            print 'P1, P2 concs:', P1conc, P2conc
+            with open(datafile,'ab+') as csvfile:
+                arduinocsv = csv.writer(csvfile, delimiter = ',')
+                arduinocsv.writerow([P2ratio, P1ratio, P1conc, P2conc, measureTime])
