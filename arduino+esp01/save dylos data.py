9f528d4e5c38be916a937b8765124380623b8c57
wordsforthewise <wordsforthewise@gmail.com>
2015-07-31 21:57:49 -0700

    moved csv open

--- arduino+esp01/save dylos data.py
+++ arduino+esp01/save dylos data.py
@@ -6,18 +6,18 @@ ser = serial.Serial(8,timeout=10)
 datafile = 'dylos data.csv'
 
 if not os.path.isfile(datafile):
-    with open(datafile,'a+') as csvfile:
+    with open(datafile,'wb') as csvfile:
         dyloscsv = csv.writer(csvfile, delimiter = ',')
         dyloscsv.writerow(['1um', '5um', 'time (iso)'])
 
-with open(datafile,'a+') as csvfile:
-    dyloscsv = csv.writer(csvfile, delimiter = ',')
-    while True:
-        line = ser.readline()
-        if line!='':
-            match = re.search('(\d+),(\d+)',line)
-            smallCount = match.group(1)
-            largeCount = match.group(2)
-            print '1um: ', smallCount, '5um: ', largeCount
-            measureTime = datetime.now().isoformat()
+while True:
+    line = ser.readline()
+    if line!='':
+        match = re.search('(\d+),(\d+)',line)
+        smallCount = match.group(1)
+        largeCount = match.group(2)
+        print '1um: ', smallCount, '5um: ', largeCount
+        measureTime = datetime.now().isoformat()
+        with open(datafile,'ab+') as csvfile:
+            dyloscsv = csv.writer(csvfile, delimiter = ',')
             dyloscsv.writerow([smallCount, largeCount, measureTime])
