326fd75094082cd6f10c4d5874efdd4b396512d5
wordsforthewise <wordsforthewise@gmail.com>
2015-07-31 20:45:01 -0700

    initial commit

---
+++ arduino+esp01/save arduino data.py
@@ -0,0 +1,23 @@
+import serial, requests, re, csv, os
+from datetime import datetime
+
+ser = serial.Serial(1,timeout=10)
+
+datafile = 'arduino data.csv'
+
+if not os.path.isfile(datafile):
+    with open(datafile,'a+') as csvfile:
+        arduinocsv = csv.writer(csvfile, delimiter = ',')
+        arduinocsv.writeline('1um', '5um', 'time (iso)')
+
+with open(datafile,'a+') as csvfile:
+    while True:
+        line = ser.readline()
+        if line!='':
+            match = re.search('(\d+),(\d+)',line)
+            smallCount = match.group(1)
+            largeCount = match.group(2)
+            print '1um: ', smallCount, '5um: ', largeCount
+            measureTime = datetime.now().isoformat()
+            arduinocsv = csv.writer(csvfile, delimiter = ',')
+            arduinocsv.writeline(smallCount, largeCount, measureTime)
