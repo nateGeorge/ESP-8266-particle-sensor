d8f77dadcc004649ec3a59e07d9d19c950bac363
wordsforthewise <wordsforthewise@gmail.com>
2015-07-31 20:54:16 -0700

    fixed re expressions

--- arduino+esp01/save arduino data.py
+++ arduino+esp01/save arduino data.py
@@ -4,20 +4,26 @@ from datetime import datetime
 ser = serial.Serial(1,timeout=10)
 
 datafile = 'arduino data.csv'
-
+P2ratio = None
 if not os.path.isfile(datafile):
     with open(datafile,'a+') as csvfile:
         arduinocsv = csv.writer(csvfile, delimiter = ',')
-        arduinocsv.writeline('0.5um', '1um', 'time (iso)')
+        arduinocsv.writerow(['0.5um', '1um', 'time (iso)'])
 
 with open(datafile,'a+') as csvfile:
+    arduinocsv = csv.writer(csvfile, delimiter = ',')
     while True:
         line = ser.readline()
         if line!='':
-            match = re.search('(\d+),(\d+)',line)
+            P1conc = re.search('P1: (\d+\.\d+)', line)
+            P1ratio = re.search('P1 ratio: (\d+\.\d+)', line)
+            P2conc = re.search('P2: (\d+\.\d+)', line)
             smallCount = match.group(1)
             largeCount = match.group(2)
             print '1um: ', smallCount, '5um: ', largeCount
             measureTime = datetime.now().isoformat()
-            arduinocsv = csv.writer(csvfile, delimiter = ',')
-            arduinocsv.writeline(smallCount, largeCount, measureTime)
+            if re.search('P2 ratio: (\d+\.\d+)', line) and P2ratio != None:
+                print 'P1, P2 ratios:', P1ratio, P2ratio
+                print 'P1, P2 concs:', P1conc, P2conc
+                P2ratio = re.search('P2 ratio: (\d+\.\d+)', line).group(1)
+                arduinocsv.writerow([smallCount, largeCount, measureTime])
