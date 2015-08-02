9b3e48d82689b7337a305da89c8cf97ad992f050
wordsforthewise <wordsforthewise@gmail.com>
2015-08-01 19:30:46 -0700

    plot by time and add labels

--- arduino+esp01/getSensorCorrelation.py
+++ arduino+esp01/getSensorCorrelation.py
@@ -1,13 +1,25 @@
 import pandas as pd
 import numpy as np
+import pylab as plt
 from datetime import datetime
 from dateutil.parser import parse
 from scipy import interp
-import pylab as plt
 from scipy.optimize import curve_fit as cf
+from dateutil import tz
+
+to_zone = tz.tzlocal()
+from_zone = tz.tzutc()
+
+mvaperiod = 20
+
+morningCutoff = datetime(2015,8,1,7)
+morningCutoff = (morningCutoff - datetime(1970,1,1)).total_seconds()
 
 def func(x, a, b, c):
     return np.power(x,3)*a + np.power(x,2)*b + x*c
+    
+def expfunc(x, a, b):
+    return np.exp(a*x)*b
 
 plt.style.use('dark_background')
 
@@ -28,17 +40,19 @@ interpTimes = []
 dylos1umData = []
 arduino1umData = []
 arduinoP1ratio = []
-for each in range(len(interpArduinoTime)):
-    if dylosTime[each] == interpArduinoTime[each]:
-        interpTimes.append(dylosTime[each])
+for each in range(len(interpArduinoTime) - mvaperiod):
+    if dylosTime[each] == interpArduinoTime[each] and dylosTime[each] > morningCutoff:
+        utc = datetime.fromtimestamp(dylosTime[each])
+        utc = utc.replace(tzinfo=from_zone)
+        interpTimes.append(utc)
         dylos1umData.append(dylosData['1um'][each])
         arduino1umData.append(interpArduinoData[each]*2.5)
-        arduinoP1ratio.append(interpArduinoRatio[each])
+        arduinoP1ratio.append(interpArduinoRatio[each + mvaperiod/2])
 rollingP1ratio = pd.rolling_mean(np.array(arduinoP1ratio),20)
 for each in range(len(rollingP1ratio)):
     if np.isnan(rollingP1ratio[each]):
         rollingP1ratio[each] = interpArduinoRatio[each]
-P1fit = np.polyfit(rollingP1ratio, dylos1umData, deg=1)
+P1fit = np.polyfit(rollingP1ratio, dylos1umData, deg=4)
 P1corr = np.poly1d(P1fit)
 minRatio = min(rollingP1ratio)
 maxRatio = max(rollingP1ratio)
@@ -48,8 +62,11 @@ print P1fit
 
 popt, pcov = cf(func, rollingP1ratio, dylos1umData)
 print popt
-fitLineX2 = np.linspace(minRatio, maxRatio, 1000)
 fitLineY2 = func(fitLineX, popt[0], popt[1], popt[2])
+
+popt3, pcov3 = cf(expfunc, rollingP1ratio, dylos1umData)
+print popt3
+fitLineY3 = expfunc(fitLineX, popt3[0], popt3[1])
 
 allData = {}
 allData['epoch time'] = interpTimes
@@ -64,8 +81,10 @@ corrData['P1 ratio'] = rollingP1ratio
 
 corrData = pd.DataFrame(corrData)
 ax = corrData.plot(kind = 'scatter', x='P1 ratio', y='dylos 1um', c='white')
-ax.plot(fitLineX, fitLineY, linewidth=3)
-ax.plot(fitLineX2, fitLineY2, linewidth=3)
+ax.plot(fitLineX, fitLineY, linewidth=3, label='linear')
+ax.plot(fitLineX, fitLineY2, linewidth=3, label='3rd order, intercept=0')
+ax.plot(fitLineX, fitLineY3, linewidth=3, label='exponential')
+ax.legend(loc='best')
 plt.show()
 
 ax = allData.plot()
@@ -74,5 +93,6 @@ plt.show()
 
 plt.scatter(allData.index, arduinoP1ratio, label='raw data')
 plt.plot(allData.index, rollingP1ratio, label='mva', c='orange', linewidth=3)
+plt.xlim(min(allData.index),max(allData.index))
 plt.legend(loc='best')
 plt.show()
