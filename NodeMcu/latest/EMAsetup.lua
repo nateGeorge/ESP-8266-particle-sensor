1146f72ab674b33be8fa7feb916737d129bcc10f
wordsforthewise <wordsforthewise@gmail.com>
2015-10-19 13:42:03 -0500

    shorten ema

--- NodeMcu/latest/EMAsetup.lua
+++ NodeMcu/latest/EMAsetup.lua
@@ -1,2 +1,2 @@
-periods = {72, 36, 12} -- /w 5s sampling, 1, 3, 6 min EMA
+periods = {36, 18, 8} -- /w 5s sampling, 1, 3, 6 min EMA
 cutoffs = {0, 100, 1000} -- 1um particle concentrations for switching periods
